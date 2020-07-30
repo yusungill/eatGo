d7f01ed9557a86e9291bf1adf9d4f0392cdb32d5
yusungill <chinggin@navcer.com>
2020-07-30 22:23:36 +0900

    DI

--- eatgo-api/src/test/java/kr/co/fastcampus/eatgo/interfaces/RestaurantControllerTest.java
+++ eatgo-api/src/test/java/kr/co/fastcampus/eatgo/interfaces/RestaurantControllerTest.java
@@ -1,9 +1,12 @@
 package kr.co.fastcampus.eatgo.interfaces;
 
+import kr.co.fastcampus.eatgo.domain.RestaurantRepository;
+import kr.co.fastcampus.eatgo.domain.RestaurantRepositoryImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -20,6 +23,8 @@ class RestaurantControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @SpyBean(RestaurantRepositoryImpl.class)
+    private RestaurantRepository restaurantRepository;
 
     @Test
     public void list(){
@@ -27,7 +32,7 @@ class RestaurantControllerTest {
             mvc.perform(get("/restaurants"))
                     .andExpect(status().isOk())
                     .andExpect(content().string(containsString("\"name\":\"Bob zip\"")))
-                    .andExpect(content().string(containsString("\"id\":1004L")));
+                    .andExpect(content().string(containsString("\"id\":1004")));
         } catch (Exception e) {
             e.printStackTrace();
         }
